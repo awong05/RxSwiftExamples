25cf3a43c68e929fa42bc36ccea55588acc0b918
Andy Wong <awong@propelmarketing.com>
2016-05-07 23:19:02 -0400

    Returning to, updating controller.

--- RxAlamofireExample/RxAlamofireExample/RepositoriesViewController.swift
+++ RxAlamofireExample/RxAlamofireExample/RepositoriesViewController.swift
@@ -16,6 +16,9 @@ class RepositoriesViewController: UIViewController
 {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var searchBar: UISearchBar!
+    let disposeBag = DisposeBag()
+    var repositoryNetworkModel: RepositoryNetworkModel!
+
     var rx_searchBarText: Observable<String> {
         return searchBar
             .rx_text
@@ -30,6 +33,29 @@ class RepositoriesViewController: UIViewController
     }
     
     func setupRx() {
-        
+        repositoryNetworkModel = RepositoryNetworkModel(withNameObservable: rx_searchBarText)
+
+        repositoryNetworkModel
+            .rx_repositories
+            .drive(tableView.rx_itemsWithCellFactory) { (tv, i, repository) in
+                let cell = tv.dequeueReusableCellWithIdentifier("repositoryCell", forIndexPath: NSIndexPath(forRow: i, inSection: 0))
+                cell.textLabel?.text = repository.name
+
+                return cell
+            }
+            .addDisposableTo(disposeBag)
+
+        repositoryNetworkModel
+            .rx_repositories
+            .driveNext { repositories in
+                if repositories.count == 0 {
+                    let alert = UIAlertController(title: ":(", message: "No repositories for this user.", preferredStyle: .Alert)
+                    alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+                    if self.navigationController?.visibleViewController?.isMemberOfClass(UIAlertController.self) != true {
+                        self.presentViewController(alert, animated: true, completion: nil)
+                    }
+                }
+            }
+            .addDisposableTo(disposeBag)
     }
 }
