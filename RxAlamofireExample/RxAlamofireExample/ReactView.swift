897a9516847ace896f407e21360b81a48bd2e9d4
Andy Wong <awong@propelmarketing.com>
2016-05-11 21:38:55 -0400

    Root View working, next to render data again.

---
+++ RxAlamofireExample/RxAlamofireExample/ReactView.swift
@@ -0,0 +1,24 @@
+//
+//  ReactView.swift
+//  RxAlamofireExample
+//
+//  Created by Andy Wong on 5/11/16.
+//  Copyright © 2016 Propel Marketing. All rights reserved.
+//
+
+import UIKit
+import React
+
+class ReactView: UIView {
+    let rootView: RCTRootView = RCTRootView(bundleURL: NSURL(string: "http://localhost:8081/React/index.ios.bundle?platform=ios"), moduleName: "App", initialProperties: nil, launchOptions: nil)
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        loadReact()
+    }
+
+    func loadReact() {
+        addSubview(rootView)
+        rootView.frame = self.bounds
+    }
+}
